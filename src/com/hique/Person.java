c22422ce2105d55752d0a6b68f40c2b8d4efd594
Manoth-2019 <manothkumar369@gmail.com>
2024-07-17 13:08:35 +0530

    mycode

---
+++ src/com/hique/Person.java
@@ -0,0 +1,31 @@
+package com.hique;
+
+public class Person {
+
+	int salaryvjmovie = 1000000;
+	String upcomemovie = "goat";
+
+	public void actor() {
+		System.out.println("leo");
+		System.out.println(upcomemovie);
+
+	}
+
+	private void politician() {
+
+		System.out.println("tvk party");
+
+	}
+
+	public static void main(String[] xyz) {
+		int salary = 5555;
+
+		Person vj = new Person();
+		vj.actor();
+		vj.politician();
+		System.out.println(vj.salaryvjmovie);
+		System.out.println(salary);
+
+	}
+
+}
